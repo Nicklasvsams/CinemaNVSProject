f068eb6a6412d70327e44d3cec564d2368e57745
Nicklas Valintin Sams <nicklasvsams@gmail.com>
2022-05-19 15:08:53 +0200

    Actor & Director repository tests

---
+++ CinemaNVS.Tests/Repository/ActorRepositoryTests.cs
@@ -0,0 +1,265 @@
+﻿using CinemaNVS.DAL.Database;
+using CinemaNVS.DAL.Database.Entities.Movies;
+using CinemaNVS.DAL.Repositories.Movies;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CinemaNVS.Tests.Repository
+{
+    public class ActorRepositoryTests
+    {
+        private readonly CinemaDBContext _dbContext;
+        private readonly IActorRepository _actorRepository;
+
+        public ActorRepositoryTests()
+        {
+            DbContextOptions<CinemaDBContext> _dbContextOptions = new DbContextOptionsBuilder<CinemaDBContext>()
+                .UseInMemoryDatabase(databaseName: "DirectorRepositoryDB")
+                .Options;
+            _dbContext = new CinemaDBContext(_dbContextOptions);
+            _actorRepository = new ActorRepository(_dbContext);
+        }
+
+        [Fact]
+        public async void SelectAllActorsAsync_ShouldReturnListOfActors_WhenActorsExist()
+        {
+            //Arrange
+            await _dbContext.Database.EnsureDeletedAsync();
+            await _dbContext.Actors.AddRangeAsync(ActorList());
+            await _dbContext.SaveChangesAsync();
+
+            //Act
+            var result = (List<Actor>)await _actorRepository.SelectAllActorsAsync();
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.Equal(2, result.Count);
+            Assert.IsType<List<Actor>>(result);
+        }
+
+        [Fact]
+        public async void SelectAllActorsAsync_ShouldReturnEmptyListOfActors_WhenNoActorsExist()
+        {
+            //Arrange
+            await _dbContext.Database.EnsureDeletedAsync();
+            await _dbContext.SaveChangesAsync();
+
+            //Act
+            var result = (List<Actor>)await _actorRepository.SelectAllActorsAsync();
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.Empty(result);
+            Assert.IsType<List<Actor>>(result);
+        }
+
+        [Fact]
+        public async void SelectActorByIdAsync_ShouldReturnSingleActor_IfActorExists()
+        {
+            //Arrange
+            int actorId = 1;
+
+            await _dbContext.Database.EnsureDeletedAsync();
+
+            _dbContext.Actors.Add(Actor());
+
+            await _dbContext.SaveChangesAsync();
+
+            //Act
+            var result = await _actorRepository.SelectActorByIdAsync(actorId);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.IsType<Actor>(result);
+            Assert.Equal(actorId, result.Id);
+            Assert.Equal("Test Name", result.Name);
+        }
+
+        [Fact]
+        public async void SelectActorByIdAsync_ShouldReturnNull_WhenActorDoesNotExist()
+        {
+            //Arrange
+            int directorId = 1;
+
+            await _dbContext.Database.EnsureDeletedAsync();
+
+            //Act
+            var result = await _actorRepository.SelectActorByIdAsync(directorId);
+
+            //Assert
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public async void DeleteActorByIdAsync_ShouldReturnDeletedActor_WhenActorExists()
+        {
+            //Arrange
+            int actorId = 1;
+
+            await _dbContext.Database.EnsureDeletedAsync();
+
+            _dbContext.Actors.Add(Actor());
+
+            await _dbContext.SaveChangesAsync();
+
+            //Act
+            var result = await _actorRepository.DeleteActorByIdAsync(actorId);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.Equal(actorId, result.Id);
+            Assert.IsType<Actor>(result);
+        }
+
+        [Fact]
+        public async void DeleteActorByIdAsync_ShouldReturnMull_WhenActorDoesNotExist()
+        {
+            //Arrange
+            int actorId = 1;
+
+            await _dbContext.Database.EnsureDeletedAsync();
+
+            //Act
+            var result = await _actorRepository.DeleteActorByIdAsync(actorId);
+
+            //Assert
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public async void InsertActorAsync_ShouldAddIdAndReturnActor_WhenActorIsInsertedSuccessfully()
+        {
+            //Arrange
+            await _dbContext.Database.EnsureDeletedAsync();
+
+            Actor actor = new Actor()
+            {
+                Name = "Test Name",
+                ImdbLink = "imdblink.dk",
+                Movies = new List<Movie>()
+            };
+
+            //Act
+            var result = await _actorRepository.InsertActorAsync(actor);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.IsType<Actor>(result);
+            Assert.Equal("Test Name", result.Name);
+            Assert.Equal(1, result.Id);
+        }
+
+        [Fact]
+        public async void InsertActorAsync_ShouldFailToAddActor_WhenActorWithSameIdAlreadyExists()
+        {
+            //Arrange
+            await _dbContext.Database.EnsureDeletedAsync();
+
+            Actor actor = new Actor()
+            {
+                Id = 1,
+                Name = "Test Name",
+                ImdbLink = "imdblink.dk",
+                Movies = new List<Movie>()
+            };
+
+            _dbContext.Actors.Add(actor);
+
+            await _dbContext.SaveChangesAsync();
+
+            //Act
+            async Task action() => await _actorRepository.InsertActorAsync(actor);
+            var ex = await Assert.ThrowsAsync<ArgumentException>(action);
+
+            //Assert
+            Assert.Contains("An item with the same key has already been added", ex.Message);
+        }
+
+        [Fact]
+        public async void UpdateActorAsync_ShouldReturnActor_WhenActorIsSuccessfullyUpdated()
+        {
+            //Arrange
+            int actorId = 1;
+
+            await _dbContext.Database.EnsureDeletedAsync();
+
+            _dbContext.Actors.Add(Actor());
+
+            await _dbContext.SaveChangesAsync();
+
+            Actor actor = new Actor()
+            {
+                Name = "Test Updatename",
+                ImdbLink = "imdblinkupdate.dk",
+                Movies = new List<Movie>()
+            };
+
+            //Act
+            var result = await _actorRepository.UpdateActorByIdAsync(actor, actorId);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.IsType<Actor>(result);
+            Assert.Equal(actorId, result.Id);
+            Assert.Equal("Test Updatename", result.Name);
+            Assert.Equal("imdblinkupdate.dk", result.ImdbLink);
+        }
+
+        [Fact]
+        public async Task UpdateActorById_ShouldReturnNull_WhenActorToUpdateDoesNotExist()
+        {
+            //Arrange
+            int actorId = 1;
+
+            await _dbContext.Database.EnsureDeletedAsync();
+
+            Actor actor = new Actor()
+            {
+                Name = "Test Updatename",
+                ImdbLink = "imdblinkupdate.dk",
+                Movies = new List<Movie>()
+            };
+
+            //Act
+            var result = await _actorRepository.UpdateActorByIdAsync(actor, actorId);
+
+            //Assert
+            Assert.Null(result);
+        }
+
+        private List<Actor> ActorList()
+        {
+            return new List<Actor>()
+            {
+                new Actor()
+                {
+                    Id = 1,
+                    Name = "Test Name",
+                    ImdbLink = "imdblink.dk",
+                    Movies = new List<Movie>()
+                },
+                new Actor()
+                {
+                    Id = 2,
+                    Name = "Test Name2",
+                    ImdbLink = "imdblink2.dk",
+                    Movies = new List<Movie>()
+                }
+            };
+        }
+
+        private Actor Actor()
+        {
+            return new Actor()
+            {
+                Id = 1,
+                Name = "Test Name",
+                ImdbLink = "imdblink.dk",
+                Movies = new List<Movie>()
+            };
+        }
+    }
+}
---
+++ CinemaNVS.Tests/Repository/DirectorRepositoryTests.cs
@@ -0,0 +1,265 @@
+﻿using CinemaNVS.DAL.Database;
+using CinemaNVS.DAL.Database.Entities.Movies;
+using CinemaNVS.DAL.Repositories.Movies;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CinemaNVS.Tests.Repository
+{
+    public class DirectorRepositoryTests
+    {
+        private readonly CinemaDBContext _dbContext;
+        private readonly IDirectorRepository _directorRepository;
+
+        public DirectorRepositoryTests()
+        {
+            DbContextOptions<CinemaDBContext> _dbContextOptions = new DbContextOptionsBuilder<CinemaDBContext>()
+                .UseInMemoryDatabase(databaseName: "DirectorRepositoryDB")
+                .Options;
+            _dbContext = new CinemaDBContext(_dbContextOptions);
+            _directorRepository = new DirectorRepository(_dbContext);
+        }
+
+        [Fact]
+        public async void SelectAllDirectorsAsync_ShouldReturnListOfDirectors_WhenDirectorsExist()
+        {
+            //Arrange
+            await _dbContext.Database.EnsureDeletedAsync();
+            await _dbContext.Directors.AddRangeAsync(DirectorList());
+            await _dbContext.SaveChangesAsync();
+
+            //Act
+            var result = (List<Director>)await _directorRepository.SelectAllDirectorsAsync();
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.Equal(2, result.Count);
+            Assert.IsType<List<Director>>(result);
+        }
+
+        [Fact]
+        public async void SelectAllDirectorsAsync_ShouldReturnEmptyListOfDirectors_WhenNoDirectorsExist()
+        {
+            //Arrange
+            await _dbContext.Database.EnsureDeletedAsync();
+            await _dbContext.SaveChangesAsync();
+
+            //Act
+            var result = (List<Director>)await _directorRepository.SelectAllDirectorsAsync();
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.Empty(result);
+            Assert.IsType<List<Director>>(result);
+        }
+
+        [Fact]
+        public async void SelectDirectorByIdAsync_ShouldReturnSingleDirector_IfDirectorExists()
+        {
+            //Arrange
+            int directorId = 1;
+
+            await _dbContext.Database.EnsureDeletedAsync();
+
+            _dbContext.Directors.Add(Director());
+
+            await _dbContext.SaveChangesAsync();
+
+            //Act
+            var result = await _directorRepository.SelectDirectorByIdAsync(directorId);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.IsType<Director>(result);
+            Assert.Equal(directorId, result.Id);
+            Assert.Equal("Test Name", result.Name);
+        }
+
+        [Fact]
+        public async void SelectDirectorByIdAsync_ShouldReturnNull_WhenDirectorDoesNotExist()
+        {
+            //Arrange
+            int directorId = 1;
+
+            await _dbContext.Database.EnsureDeletedAsync();
+
+            //Act
+            var result = await _directorRepository.SelectDirectorByIdAsync(directorId);
+
+            //Assert
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public async void DeleteDirectorByIdAsync_ShouldReturnDeletedDirector_WhenDirectorExists()
+        {
+            //Arrange
+            int directorId = 1;
+
+            await _dbContext.Database.EnsureDeletedAsync();
+
+            _dbContext.Directors.Add(Director());
+
+            await _dbContext.SaveChangesAsync();
+
+            //Act
+            var result = await _directorRepository.DeleteDirectorByIdAsync(directorId);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.Equal(directorId, result.Id);
+            Assert.IsType<Director>(result);
+        }
+
+        [Fact]
+        public async void DeleteDirectorByIdAsync_ShouldReturnMull_WhenDirectorDoesNotExist()
+        {
+            //Arrange
+            int directorId = 1;
+
+            await _dbContext.Database.EnsureDeletedAsync();
+
+            //Act
+            var result = await _directorRepository.DeleteDirectorByIdAsync(directorId);
+
+            //Assert
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public async void InsertDirectorAsync_ShouldAddIdAndReturnDirector_WhenDirectorIsInsertedSuccessfully()
+        {
+            //Arrange
+            await _dbContext.Database.EnsureDeletedAsync();
+
+            Director director = new Director()
+            {
+                Name = "Test Name",
+                ImdbLink = "imdblink.dk",
+                Movies = new List<Movie>()
+            };
+
+            //Act
+            var result = await _directorRepository.InsertDirectorAsync(director);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.IsType<Director>(result);
+            Assert.Equal("Test Name", result.Name);
+            Assert.Equal(1, result.Id);
+        }
+
+        [Fact]
+        public async void InsertDirectorAsync_ShouldFailToAddDirector_WhenDirectorWithSameIdAlreadyExists()
+        {
+            //Arrange
+            await _dbContext.Database.EnsureDeletedAsync();
+
+            Director director = new Director()
+            {
+                Id = 1,
+                Name = "Test Name",
+                ImdbLink = "imdblink.dk",
+                Movies = new List<Movie>()
+            };
+
+            _dbContext.Directors.Add(director);
+
+            await _dbContext.SaveChangesAsync();
+
+            //Act
+            async Task action() => await _directorRepository.InsertDirectorAsync(director);
+            var ex = await Assert.ThrowsAsync<ArgumentException>(action);
+
+            //Assert
+            Assert.Contains("An item with the same key has already been added", ex.Message);
+        }
+
+        [Fact]
+        public async void UpdateDirectorAsync_ShouldReturnDirector_WhenDirectorIsSuccessfullyUpdated()
+        {
+            //Arrange
+            int directorId = 1;
+
+            await _dbContext.Database.EnsureDeletedAsync();
+
+            _dbContext.Directors.Add(Director());
+
+            await _dbContext.SaveChangesAsync();
+
+            Director director = new Director()
+            {
+                Name = "Test Updatename",
+                ImdbLink = "imdblinkupdate.dk",
+                Movies = new List<Movie>()
+            };
+
+            //Act
+            var result = await _directorRepository.UpdateDirectorByIdAsync(director, directorId);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.IsType<Director>(result);
+            Assert.Equal(directorId, result.Id);
+            Assert.Equal("Test Updatename", result.Name);
+            Assert.Equal("imdblinkupdate.dk", result.ImdbLink);
+        }
+
+        [Fact]
+        public async Task UpdateDirectorById_ShouldReturnNull_WhenDirectorToUpdateDoesNotExist()
+        {
+            //Arrange
+            int directorId = 1;
+
+            await _dbContext.Database.EnsureDeletedAsync();
+
+            Director director = new Director()
+            {
+                Name = "Test Updatename",
+                ImdbLink = "imdblinkupdate.dk",
+                Movies = new List<Movie>()
+            };
+
+            //Act
+            var result = await _directorRepository.UpdateDirectorByIdAsync(director, directorId);
+
+            //Assert
+            Assert.Null(result);
+        }
+
+        private List<Director> DirectorList()
+        {
+            return new List<Director>()
+            {
+                new Director()
+                {
+                    Id = 1,
+                    Name = "Test Name",
+                    ImdbLink = "imdblink.dk",
+                    Movies = new List<Movie>()
+                },
+                new Director()
+                {
+                    Id = 2,
+                    Name = "Test Name2",
+                    ImdbLink = "imdblink2.dk",
+                    Movies = new List<Movie>()
+                }
+            };
+        }
+
+        private Director Director()
+        {
+            return new Director()
+            {
+                Id = 1,
+                Name = "Test Name",
+                ImdbLink = "imdblink.dk",
+                Movies = new List<Movie>()
+            };
+        }
+    }
+}
