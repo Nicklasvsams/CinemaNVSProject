7994fa828e84b6cd33b736fd112b93e2a920a790
Nicklas Valintin Sams <nicklasvsams@gmail.com>
2022-05-19 15:53:18 +0200

    Actor & Director tests implemented

---
+++ CinemaNVS.Tests/Controllers/ActorControllerTests.cs
@@ -0,0 +1,327 @@
+﻿using CinemaNVS.Controllers;
+using CinemasNVS.BLL.DTOs;
+using CinemasNVS.BLL.Services.MovieServices;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Moq;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace CinemaNVS.Tests.Controllers
+{
+    public class ActorControllerTests
+    {
+        private readonly Mock<IActorService> _mockActorService = new Mock<IActorService>();
+        private readonly ActorController _actorController;
+
+        public ActorControllerTests()
+        {
+            _actorController = new ActorController(_mockActorService.Object);
+        }
+
+        [Fact]
+        public async void GetAll_ShouldReturnStatusCode200_WhenActorsExist()
+        {
+            // Arrange
+            List<ActorResponse> actors = new List<ActorResponse>();
+
+            actors.AddRange(ActorResponseList());
+
+            _mockActorService.Setup(x => x.GetAllActorsAsync())
+                .ReturnsAsync(actors);
+
+            // Act
+            var result = await _actorController.GetAll();
+            var statusCodeResult = (IStatusCodeActionResult)result;
+
+            // Assert
+            Assert.Equal(200, statusCodeResult.StatusCode);
+        }
+
+        [Fact]
+        public async void GetAll_ShouldReturnStatusCode204_WhenNoActorsExist()
+        {
+            // Arrange
+            List<ActorResponse> actors = new List<ActorResponse>();
+
+            _mockActorService.Setup(x => x.GetAllActorsAsync())
+                .ReturnsAsync(actors);
+
+            // Act
+            var result = await _actorController.GetAll();
+            var statusCodeResult = (IStatusCodeActionResult)result;
+
+            // Assert
+            Assert.Equal(204, statusCodeResult.StatusCode);
+        }
+
+        [Fact]
+        public async void GetAll_ShouldReturnStatusCode500_WhenServiceReturnsNull()
+        {
+            // Arrange
+            List<ActorResponse> games = new List<ActorResponse>();
+
+            _mockActorService
+                .Setup(x => x.GetAllActorsAsync())
+                .ReturnsAsync(() => null);
+
+            // Act
+            var result = await _actorController.GetAll();
+            var statusCodeResult = (IStatusCodeActionResult)result;
+
+            // Assert
+            Assert.Equal(500, statusCodeResult.StatusCode);
+        }
+
+        [Fact]
+        public async void GetAll_ShouldReturnStatusCode500_WhenExceptionIsRaised()
+        {
+            // Arrange
+            _mockActorService
+                .Setup(x => x.GetAllActorsAsync())
+                .ReturnsAsync(() => throw new Exception("test"));
+
+            // Act
+            var result = await _actorController.GetAll();
+            var statusCodeResult = (IStatusCodeActionResult)result;
+
+            // Assert
+            Assert.Equal(500, statusCodeResult.StatusCode);
+        }
+
+        [Fact]
+        public async void GetById_ShouldReturnStatusCode200_WhenActorExists()
+        {
+            // Arrange
+            int actorId = 1;
+
+            _mockActorService
+                .Setup(x => x.GetActorByIdAsync(It.IsAny<int>()))
+                .ReturnsAsync(ActorResponse());
+
+            // Act
+            var result = await _actorController.GetById(actorId);
+            var statusCodeResult = (IStatusCodeActionResult)result;
+
+            // Assert
+            Assert.Equal(200, statusCodeResult.StatusCode);
+        }
+
+        [Fact]
+        public async void GetById_ShouldReturnStatusCode500_WhenExceptionIsRaised()
+        {
+            // Arrange
+            int actorId = 1;
+
+            _mockActorService
+                .Setup(x => x.GetActorByIdAsync(It.IsAny<int>()))
+                .ReturnsAsync(() => throw new Exception("Test"));
+
+            // Act
+            var result = await _actorController.GetById(actorId);
+            var statusCodeResult = (IStatusCodeActionResult)result;
+
+            // Assert
+            Assert.Equal(500, statusCodeResult.StatusCode);
+        }
+
+        [Fact]
+        public async void GetById_ShouldReturnStatusCode404_WhenActorDoesNotExist()
+        {
+            // Arrange
+            int actorId = 1;
+
+            _mockActorService
+                .Setup(x => x.GetActorByIdAsync(actorId))
+                .ReturnsAsync(() => null);
+
+            // Act
+            var result = await _actorController.GetById(actorId);
+            var statusCodeResult = (IStatusCodeActionResult)result;
+
+            // Assert
+            Assert.Equal(404, statusCodeResult.StatusCode);
+        }
+
+        [Fact]
+        public async void Create_ShouldReturnStatusCode200_WhenActorIsCreated()
+        {
+            // Arrange
+            _mockActorService
+                .Setup(x => x.CreateActorAsync(It.IsAny<ActorRequest>()))
+                .ReturnsAsync(ActorResponse());
+
+            // Act
+            var result = await _actorController.Create(ActorRequest());
+            var statusCodeResult = (IStatusCodeActionResult)result;
+
+            // Assert
+            Assert.Equal(200, statusCodeResult.StatusCode);
+        }
+
+        [Fact]
+        public async void Create_ShouldReturnStatusCode500_WhenExceptionIsRaised()
+        {
+            // Arrange
+            _mockActorService
+                .Setup(x => x.CreateActorAsync(It.IsAny<ActorRequest>()))
+                .ReturnsAsync(() => throw new Exception("Test"));
+
+            // Act
+            var result = await _actorController.Create(ActorRequest());
+            var statusCodeResult = (IStatusCodeActionResult)result;
+
+            // Assert
+            Assert.Equal(500, statusCodeResult.StatusCode);
+        }
+
+        [Fact]
+        public async void Update_ShouldReturnStatusCode200_WhenUpdateIsSuccessful()
+        {
+            // Arrange
+            int actorId = 1;
+
+            _mockActorService
+                .Setup(x => x.UpdateActorByIdAsync(It.IsAny<int>(), It.IsAny<ActorRequest>()))
+                .ReturnsAsync(ActorResponse());
+
+            // Act
+            var result = await _actorController.Update(actorId, ActorRequest());
+            var statusCodeResult = (IStatusCodeActionResult)result;
+
+            // Assert
+            Assert.Equal(200, statusCodeResult.StatusCode);
+        }
+
+        [Fact]
+        public async void Update_ShouldReturnStatusCode404_WhenActorToUpdateIsNotFound()
+        {
+            // Arrange
+            int actorId = 1;
+
+            _mockActorService
+                .Setup(x => x.UpdateActorByIdAsync(It.IsAny<int>(), It.IsAny<ActorRequest>()))
+                .ReturnsAsync(() => null);
+
+            // Act
+            var result = await _actorController.Update(actorId, ActorRequest());
+            var statusCodeResult = (IStatusCodeActionResult)result;
+
+            // Assert
+            Assert.Equal(404, statusCodeResult.StatusCode);
+        }
+
+        [Fact]
+        public async void Update_ShouldReturnStatusCode500_WhenExceptionIsRaised()
+        {
+            // Arrange
+            int actorId = 1;
+
+            _mockActorService
+                .Setup(x => x.UpdateActorByIdAsync(It.IsAny<int>(), It.IsAny<ActorRequest>()))
+                .ReturnsAsync(() => throw new Exception("Test"));
+
+            // Act
+            var result = await _actorController.Update(actorId, ActorRequest());
+            var statusCodeResult = (IStatusCodeActionResult)result;
+
+            // Assert
+            Assert.Equal(500, statusCodeResult.StatusCode);
+        }
+
+        [Fact]
+        public async void Delete_ShouldReturnStatusCode200_WhenDeleteIsSuccessful()
+        {
+            // Arrange
+            int actorId = 1;
+
+            _mockActorService
+                .Setup(x => x.DeleteActorByIdAsync(It.IsAny<int>()))
+                .ReturnsAsync(ActorResponse());
+
+            // Act
+            var result = await _actorController.Delete(actorId);
+            var statusCodeResult = (IStatusCodeActionResult)result;
+
+            // Assert
+            Assert.Equal(200, statusCodeResult.StatusCode);
+        }
+
+        [Fact]
+        public async void Delete_ShouldReturnStatusCode404_WhenActorToDeleteIsNotFound()
+        {
+            // Arrange
+            int actorId = 1;
+
+            _mockActorService
+                .Setup(x => x.DeleteActorByIdAsync(It.IsAny<int>()))
+                .ReturnsAsync(() => null);
+
+            // Act
+            var result = await _actorController.Delete(actorId);
+            var statusCodeResult = (IStatusCodeActionResult)result;
+
+            // Assert
+            Assert.Equal(404, statusCodeResult.StatusCode);
+        }
+
+        [Fact]
+        public async void Delete_ShouldReturnStatusCode500_WhenExceptionIsRaised()
+        {
+            // Arrange
+            int actorId = 1;
+
+            _mockActorService
+                .Setup(x => x.DeleteActorByIdAsync(It.IsAny<int>()))
+                .ReturnsAsync(() => throw new Exception("Test"));
+
+            // Act
+            var result = await _actorController.Delete(actorId);
+            var statusCodeResult = (IStatusCodeActionResult)result;
+
+            // Assert
+            Assert.Equal(500, statusCodeResult.StatusCode);
+        }
+
+        private IEnumerable<ActorResponse> ActorResponseList()
+        {
+            return new List<ActorResponse>()
+            {
+                new ActorResponse()
+                {
+                    Id = 1,
+                    Name = "Test Name",
+                    ImdbLink = "imdblink.dk",
+                    Movies = new List<ActorMovieResponse>()
+                },
+                new ActorResponse()
+                {
+                    Id = 2,
+                    Name = "Test Name2",
+                    ImdbLink = "imdblink2.dk",
+                    Movies = new List<ActorMovieResponse>()
+                }
+            };
+        }
+
+        private ActorResponse ActorResponse()
+        {
+            return new ActorResponse()
+            {
+                Id = 1,
+                Name = "Test Name",
+                ImdbLink = "imdblink.dk",
+                Movies = new List<ActorMovieResponse>()
+            };
+        }
+
+        private ActorRequest ActorRequest()
+        {
+            return new ActorRequest()
+            {
+                Name = "Test Name",
+                ImdbLink = "imdblink.dk",
+            };
+        }
+    }
+}
---
+++ CinemaNVS.Tests/Controllers/DirectorControllerTests.cs
@@ -0,0 +1,327 @@
+﻿using CinemaNVS.Controllers;
+using CinemasNVS.BLL.DTOs;
+using CinemasNVS.BLL.Services.MovieServices;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Moq;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace CinemaNVS.Tests.Controllers
+{
+    public class DirectorControllerTests
+    {
+        private readonly Mock<IDirectorService> _mockDirectorService = new Mock<IDirectorService>();
+        private readonly DirectorController _directorController;
+
+        public DirectorControllerTests()
+        {
+            _directorController = new DirectorController(_mockDirectorService.Object);
+        }
+
+        [Fact]
+        public async void GetAll_ShouldReturnStatusCode200_WhenDirectorsExist()
+        {
+            // Arrange
+            List<DirectorResponse> directors = new List<DirectorResponse>();
+
+            directors.AddRange(DirectorResponseList());
+
+            _mockDirectorService.Setup(x => x.GetAllDirectorsAsync())
+                .ReturnsAsync(directors);
+
+            // Act
+            var result = await _directorController.GetAll();
+            var statusCodeResult = (IStatusCodeActionResult)result;
+
+            // Assert
+            Assert.Equal(200, statusCodeResult.StatusCode);
+        }
+
+        [Fact]
+        public async void GetAll_ShouldReturnStatusCode204_WhenNoDirectorsExist()
+        {
+            // Arrange
+            List<DirectorResponse> directors = new List<DirectorResponse>();
+
+            _mockDirectorService.Setup(x => x.GetAllDirectorsAsync())
+                .ReturnsAsync(directors);
+
+            // Act
+            var result = await _directorController.GetAll();
+            var statusCodeResult = (IStatusCodeActionResult)result;
+
+            // Assert
+            Assert.Equal(204, statusCodeResult.StatusCode);
+        }
+
+        [Fact]
+        public async void GetAll_ShouldReturnStatusCode500_WhenServiceReturnsNull()
+        {
+            // Arrange
+            List<DirectorResponse> games = new List<DirectorResponse>();
+
+            _mockDirectorService
+                .Setup(x => x.GetAllDirectorsAsync())
+                .ReturnsAsync(() => null);
+
+            // Act
+            var result = await _directorController.GetAll();
+            var statusCodeResult = (IStatusCodeActionResult)result;
+
+            // Assert
+            Assert.Equal(500, statusCodeResult.StatusCode);
+        }
+
+        [Fact]
+        public async void GetAll_ShouldReturnStatusCode500_WhenExceptionIsRaised()
+        {
+            // Arrange
+            _mockDirectorService
+                .Setup(x => x.GetAllDirectorsAsync())
+                .ReturnsAsync(() => throw new Exception("test"));
+
+            // Act
+            var result = await _directorController.GetAll();
+            var statusCodeResult = (IStatusCodeActionResult)result;
+
+            // Assert
+            Assert.Equal(500, statusCodeResult.StatusCode);
+        }
+
+        [Fact]
+        public async void GetById_ShouldReturnStatusCode200_WhenDirectorExists()
+        {
+            // Arrange
+            int directorId = 1;
+
+            _mockDirectorService
+                .Setup(x => x.GetDirectorByIdAsync(It.IsAny<int>()))
+                .ReturnsAsync(DirectorResponse());
+
+            // Act
+            var result = await _directorController.GetById(directorId);
+            var statusCodeResult = (IStatusCodeActionResult)result;
+
+            // Assert
+            Assert.Equal(200, statusCodeResult.StatusCode);
+        }
+
+        [Fact]
+        public async void GetById_ShouldReturnStatusCode500_WhenExceptionIsRaised()
+        {
+            // Arrange
+            int directorId = 1;
+
+            _mockDirectorService
+                .Setup(x => x.GetDirectorByIdAsync(It.IsAny<int>()))
+                .ReturnsAsync(() => throw new Exception("Test"));
+
+            // Act
+            var result = await _directorController.GetById(directorId);
+            var statusCodeResult = (IStatusCodeActionResult)result;
+
+            // Assert
+            Assert.Equal(500, statusCodeResult.StatusCode);
+        }
+
+        [Fact]
+        public async void GetById_ShouldReturnStatusCode404_WhenDirectorDoesNotExist()
+        {
+            // Arrange
+            int directorId = 1;
+
+            _mockDirectorService
+                .Setup(x => x.GetDirectorByIdAsync(directorId))
+                .ReturnsAsync(() => null);
+
+            // Act
+            var result = await _directorController.GetById(directorId);
+            var statusCodeResult = (IStatusCodeActionResult)result;
+
+            // Assert
+            Assert.Equal(404, statusCodeResult.StatusCode);
+        }
+
+        [Fact]
+        public async void Create_ShouldReturnStatusCode200_WhenDirectorIsCreated()
+        {
+            // Arrange
+            _mockDirectorService
+                .Setup(x => x.CreateDirectorAsync(It.IsAny<DirectorRequest>()))
+                .ReturnsAsync(DirectorResponse());
+
+            // Act
+            var result = await _directorController.Create(DirectorRequest());
+            var statusCodeResult = (IStatusCodeActionResult)result;
+
+            // Assert
+            Assert.Equal(200, statusCodeResult.StatusCode);
+        }
+
+        [Fact]
+        public async void Create_ShouldReturnStatusCode500_WhenExceptionIsRaised()
+        {
+            // Arrange
+            _mockDirectorService
+                .Setup(x => x.CreateDirectorAsync(It.IsAny<DirectorRequest>()))
+                .ReturnsAsync(() => throw new Exception("Test"));
+
+            // Act
+            var result = await _directorController.Create(DirectorRequest());
+            var statusCodeResult = (IStatusCodeActionResult)result;
+
+            // Assert
+            Assert.Equal(500, statusCodeResult.StatusCode);
+        }
+
+        [Fact]
+        public async void Update_ShouldReturnStatusCode200_WhenUpdateIsSuccessful()
+        {
+            // Arrange
+            int directorId = 1;
+
+            _mockDirectorService
+                .Setup(x => x.UpdateDirectorByIdAsync(It.IsAny<int>(), It.IsAny<DirectorRequest>()))
+                .ReturnsAsync(DirectorResponse());
+
+            // Act
+            var result = await _directorController.Update(directorId, DirectorRequest());
+            var statusCodeResult = (IStatusCodeActionResult)result;
+
+            // Assert
+            Assert.Equal(200, statusCodeResult.StatusCode);
+        }
+
+        [Fact]
+        public async void Update_ShouldReturnStatusCode404_WhenDirectorToUpdateIsNotFound()
+        {
+            // Arrange
+            int directorId = 1;
+
+            _mockDirectorService
+                .Setup(x => x.UpdateDirectorByIdAsync(It.IsAny<int>(), It.IsAny<DirectorRequest>()))
+                .ReturnsAsync(() => null);
+
+            // Act
+            var result = await _directorController.Update(directorId, DirectorRequest());
+            var statusCodeResult = (IStatusCodeActionResult)result;
+
+            // Assert
+            Assert.Equal(404, statusCodeResult.StatusCode);
+        }
+
+        [Fact]
+        public async void Update_ShouldReturnStatusCode500_WhenExceptionIsRaised()
+        {
+            // Arrange
+            int directorId = 1;
+
+            _mockDirectorService
+                .Setup(x => x.UpdateDirectorByIdAsync(It.IsAny<int>(), It.IsAny<DirectorRequest>()))
+                .ReturnsAsync(() => throw new Exception("Test"));
+
+            // Act
+            var result = await _directorController.Update(directorId, DirectorRequest());
+            var statusCodeResult = (IStatusCodeActionResult)result;
+
+            // Assert
+            Assert.Equal(500, statusCodeResult.StatusCode);
+        }
+
+        [Fact]
+        public async void Delete_ShouldReturnStatusCode200_WhenDeleteIsSuccessful()
+        {
+            // Arrange
+            int directorId = 1;
+
+            _mockDirectorService
+                .Setup(x => x.DeleteDirectorByIdAsync(It.IsAny<int>()))
+                .ReturnsAsync(DirectorResponse());
+
+            // Act
+            var result = await _directorController.Delete(directorId);
+            var statusCodeResult = (IStatusCodeActionResult)result;
+
+            // Assert
+            Assert.Equal(200, statusCodeResult.StatusCode);
+        }
+
+        [Fact]
+        public async void Delete_ShouldReturnStatusCode404_WhenDirectorToDeleteIsNotFound()
+        {
+            // Arrange
+            int directorId = 1;
+
+            _mockDirectorService
+                .Setup(x => x.DeleteDirectorByIdAsync(It.IsAny<int>()))
+                .ReturnsAsync(() => null);
+
+            // Act
+            var result = await _directorController.Delete(directorId);
+            var statusCodeResult = (IStatusCodeActionResult)result;
+
+            // Assert
+            Assert.Equal(404, statusCodeResult.StatusCode);
+        }
+
+        [Fact]
+        public async void Delete_ShouldReturnStatusCode500_WhenExceptionIsRaised()
+        {
+            // Arrange
+            int directorId = 1;
+
+            _mockDirectorService
+                .Setup(x => x.DeleteDirectorByIdAsync(It.IsAny<int>()))
+                .ReturnsAsync(() => throw new Exception("Test"));
+
+            // Act
+            var result = await _directorController.Delete(directorId);
+            var statusCodeResult = (IStatusCodeActionResult)result;
+
+            // Assert
+            Assert.Equal(500, statusCodeResult.StatusCode);
+        }
+
+        private IEnumerable<DirectorResponse> DirectorResponseList()
+        {
+            return new List<DirectorResponse>()
+            {
+                new DirectorResponse()
+                {
+                    Id = 1,
+                    Name = "Test Name",
+                    ImdbLink = "imdblink.dk",
+                    Movies = new List<DirectorMovieResponse>()
+                },
+                new DirectorResponse()
+                {
+                    Id = 2,
+                    Name = "Test Name2",
+                    ImdbLink = "imdblink2.dk",
+                    Movies = new List<DirectorMovieResponse>()
+                }
+            };
+        }
+
+        private DirectorResponse DirectorResponse()
+        {
+            return new DirectorResponse()
+            {
+                Id = 1,
+                Name = "Test Name",
+                ImdbLink = "imdblink.dk",
+                Movies = new List<DirectorMovieResponse>()
+            };
+        }
+
+        private DirectorRequest DirectorRequest()
+        {
+            return new DirectorRequest()
+            {
+                Name = "Test Name",
+                ImdbLink = "imdblink.dk",
+            };
+        }
+    }
+}
