741fae4c9270ece8be15c51f287557727fa00e8a
Nicklas Valintin Sams <nicklasvsams@gmail.com>
2022-06-04 16:50:09 +0200

    Implemented Customer controller endpoint

--- CinemaNVS/Controllers/CustomerController.cs
+++ CinemaNVS/Controllers/CustomerController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CinemasNVS.BLL.DTOs;
+using CinemasNVS.BLL.Services.UserServices;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace CinemaNVS.Controllers
 {
@@ -7,6 +11,130 @@ namespace CinemaNVS.Controllers
     [ApiController]
     public class CustomerController : ControllerBase
     {
-        // TODO: Implement
+        private readonly ICustomerService _customerService;
+
+        public CustomerController(ICustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetAll()
+        {
+            try
+            {
+                List<CustomerResponse> customers = (List<CustomerResponse>)await _customerService.GetAllCustomersAsync();
+
+                if (customers == null)
+                {
+                    return StatusCode(500);
+                }
+
+                if (customers.Count == 0)
+                {
+                    return NoContent();
+                }
+
+                return Ok(customers);
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetById([FromRoute] int id)
+        {
+            try
+            {
+                var customerResponse = await _customerService.GetCustomerByIdAsync(id);
+
+                if (customerResponse == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(customerResponse);
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Create([FromBody] CustomerRequest cusReq)
+        {
+            try
+            {
+                var customerResponse = await _customerService.CreateCustomer(cusReq);
+
+                if (customerResponse == null)
+                {
+                    return StatusCode(500);
+                }
+
+                return Ok(customerResponse);
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Update([FromBody] CustomerRequest cusReq, [FromRoute] int id)
+        {
+            try
+            {
+                var customerResponse = await _customerService.UpdateCustomerByIdAsync(cusReq, id);
+
+                if (customerResponse == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(customerResponse);
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> UpdateActivation([FromRoute] int id)
+        {
+            try
+            {
+                var customerResponse = await _customerService.UpdateCustomerActivationByIdAsync(id);
+
+                if (customerResponse == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(customerResponse);
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
     }
 }
